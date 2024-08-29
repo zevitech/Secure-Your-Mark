73b4c9f2d7ae1d61fcbc3641eca6ac2006d55668
mahfujul-alam-anik <mahfujulalam.anik@gmail.com>
2024-08-30 03:07:50 +0600

    fixed r-2

---
+++ app/(main)/trademark-registration/loading.js
@@ -0,0 +1,12 @@
+import PageLoader from "@/components/ui/PageLoader";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <PageLoader />
+    </div>
+  );
+};
+
+export default page;
