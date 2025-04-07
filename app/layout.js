e1cf898a3c08fb3521d291f74f0409b1b2198eec
Zevitech-Development <zevitechdevelopment@gmail.com>
2025-04-08 02:18:00 +0500

    made changes

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import "./globals.css";
 import { Poppins, Inria_Serif } from "next/font/google";
 import Script from "next/script";
-import GlobalProvider from "./(main)/trademark-register/GlobalProvider";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -57,7 +56,8 @@ export default function RootLayout({ children }) {
     window.__lc.license = 19119842;
     window.__lc.integration_name = "manual_onboarding";
     window.__lc.product_name = "livechat";
-    ;(function(n,t,c){function i(n){return e.h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n._lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))`,
+    ;(function(n,t,c){function i(n){return e.h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n._lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))
+`,
           }}
         />
 
