2b910a07e8e437cdeae5e423998611d9d83b36bb
Zevitech-Development <zevitechdevelopment@gmail.com>
2025-04-08 02:50:51 +0500

    made changes regardinglive chat

--- app/(main)/trademark-register/layout.js
+++ app/(main)/trademark-register/layout.js
@@ -15,21 +15,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* Live Chat */}
-        <Script
-          id="livechat-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-           window.__lc = window.__lc || {};
-    window.__lc.license = 19119842;
-    window.__lc.integration_name = "manual_channels";
-    window.__lc.product_name = "livechat";
-    ;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n.__lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))
-`,
-          }}
-        />
-
         {/* Google Tag Manager */}
         <Script
           id="gtm"
@@ -90,7 +75,7 @@ export default function RootLayout({ children }) {
         </noscript>
 
         {/* Live Chat (noscript) */}
-        <noscript>
+        {/* <noscript>
           <a href="https://www.livechat.com/chat-with/19119842/" rel="nofollow">
             Chat with us
           </a>
@@ -102,12 +87,27 @@ export default function RootLayout({ children }) {
           >
             LiveChat
           </a>
-        </noscript>
+        </noscript> */}
         {/* End Live Chat (noscript) */}
 
         <div className={`bg-form-body`}></div>
 
         <GlobalProvider>{children}</GlobalProvider>
+
+        {/* Live Chat */}
+        <Script
+          id="livechat-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+           window.__lc = window.__lc || {};
+    window.__lc.license = 19119842;
+    window.__lc.integration_name = "manual_channels";
+    window.__lc.product_name = "livechat";
+    ;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n.__lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))
+`,
+          }}
+        />
       </body>
     </html>
   );
--- app/layout.js
+++ app/layout.js
@@ -46,21 +46,6 @@ export default function RootLayout({ children }) {
           }}
         />
 
-        {/* Live Chat */}
-        <Script
-          id="livechat-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-          window.__lc = window.__lc || {};
-    window.__lc.license = 19119842;
-    window.__lc.integration_name = "manual_channels";
-    window.__lc.product_name = "livechat";
-    ;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n.__lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))
-`,
-          }}
-        />
-
         {/* Google Tag Manager */}
         <Script
           id="gtm"
@@ -121,7 +106,7 @@ export default function RootLayout({ children }) {
         {/* End Google Tag Manager (noscript) */}
 
         {/* Live Chat (noscript) */}
-        <noscript>
+        {/* <noscript>
           <a href="https://www.livechat.com/chat-with/19119842/" rel="nofollow">
             Chat with us
           </a>
@@ -133,10 +118,25 @@ export default function RootLayout({ children }) {
           >
             LiveChat
           </a>
-        </noscript>
+        </noscript> */}
         {/* End Live Chat (noscript) */}
 
         {children}
+
+        {/* Live Chat */}
+        <Script
+          id="livechat-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.__lc = window.__lc || {};
+    window.__lc.license = 19119842;
+    window.__lc.integration_name = "manual_channels";
+    window.__lc.product_name = "livechat";
+    ;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n.__lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))
+`,
+          }}
+        />
       </body>
     </html>
   );
