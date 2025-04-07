b39ad8b18f395b9b0a211834cf130d2bd27f1568
Zevitech-Development <zevitechdevelopment@gmail.com>
2025-04-07 21:14:47 +0500

    added new google tag

--- app/layout.js
+++ app/layout.js
@@ -24,25 +24,32 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* Microsoft UET Tag */}
-      <Script
-        id="microsoftUET"
-        type="text/javascript"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+      <head>
+        <meta
+          name="keywords"
+          content="where to register business name, trademark my name, trademark brand name, apply for trademark online, trade mark a company name, file my trademark, brand name trademark registration, best trademark registration service, trademark filing company, cheap trademark registration, trade mark registration, get a trademark, register my business, register my company name, trademark my logo, trademark my business name, apply for trademark, file trademark for business name, trademark registration, register my business name, register business name, trademark a name, file a trademark, get my brand trademarked, trademark and brand registration"
+        />
+        <meta name="author" content="Secure Your Mark" />
+
+        {/* Microsoft UET Tag */}
+        <Script
+          id="microsoftUET"
+          type="text/javascript"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
               (function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"343156876", enableAutoSpaTracking: true};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","//bat.bing.com/bat.js","uetq");
             `,
-        }}
-      />
+          }}
+        />
 
-      {/* Tawk.to */}
-      <Script
-        id="tawkTo"
-        type="text/javascript"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+        {/* Tawk.to */}
+        <Script
+          id="tawkTo"
+          type="text/javascript"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
             var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
             (function(){
               var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -53,30 +60,30 @@ export default function RootLayout({ children }) {
               s0.parentNode.insertBefore(s1,s0);
             })();
           `,
-        }}
-      />
+          }}
+        />
 
-      {/* Google Tag Manager */}
-      <Script
-        id="gtm"
-        type="text/javascript"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm"
+          type="text/javascript"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-PXZMCZM2');`,
-        }}
-      />
+          }}
+        />
 
-      {/* MouseFlow Tracking */}
-      <Script
-        id="mouseflow"
-        type="text/javascript"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+        {/* MouseFlow Tracking */}
+        <Script
+          id="mouseflow"
+          type="text/javascript"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
             window._mfq = window._mfq || [];
             (function() {
               var mf = document.createElement("script");
@@ -86,16 +93,24 @@ export default function RootLayout({ children }) {
               document.getElementsByTagName("head")[0].appendChild(mf);
             })();
            `,
-        }}
-      />
-
-      <head>
-        <meta
-          name="keywords"
-          content="where to register business name, trademark my name, trademark brand name, apply for trademark online, trade mark a company name, file my trademark, brand name trademark registration, best trademark registration service, trademark filing company, cheap trademark registration, trade mark registration, get a trademark, register my business, register my company name, trademark my logo, trademark my business name, apply for trademark, file trademark for business name, trademark registration, register my business name, register business name, trademark a name, file a trademark, get my brand trademarked, trademark and brand registration"
+          }}
         />
-        <meta name="author" content="Secure Your Mark" />
+
+        {/* New Google Tag Manager Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16979187198"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16979187198');
+          `}
+        </Script>
       </head>
+
       <body className={`${poppins.className} ${inriaSerif.variable}`}>
         {/* Google Tag Manager (noscript) */}
         <noscript>
