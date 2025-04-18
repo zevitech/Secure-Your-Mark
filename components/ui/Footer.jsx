3baaca33fe366d257d6bb5e6ab60545748d0553d
Zevitech-Development <zevitechdevelopment@gmail.com>
2025-04-19 04:04:10 +0500

    made changes

--- components/ui/Footer.jsx
+++ components/ui/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
           </p>
           <div className="flex items-center gap-5 mt-7 max-md:mt-3 max-md:flex-col max-md:items-start">
             <div className="flex flex-col gap-4">
-              <Link href="tel:+14082562372" className="flex-center">
+              <Link href="tel:+14082562372" className="flex items-center">
                 <Image
                   src={`/images/orange-phone-icon.png`}
                   alt="Bell"
@@ -37,7 +37,7 @@ const Footer = () => {
                 </div>
               </Link>
 
-              <Link href="tel:+14082563271" className="flex-center">
+              <Link href="tel:+14082563271" className="flex items-center">
                 <Image
                   src={`/images/orange-phone-icon.png`}
                   alt="Bell"
@@ -53,8 +53,8 @@ const Footer = () => {
                 </div>
               </Link>
 
-              <div className="flex-center">
-                <IoPin size={24} className="text-[#F25601]" />
+              <div className="flex items-center">
+                <IoPin size={30} className="text-[#F25601]" />
                 <div className="flex flex-col items-center">
                   <h4 className="text-color-primary text-base font-semibold">
                     1060 Lincoln Ave Suite 20 #1071, San Jose, CA 95125
